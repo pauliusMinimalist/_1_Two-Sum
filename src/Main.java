666ed9cb7690777616059dedcb1e1976bea107cc
BEAAST <paulius.remeika00@gmail.com>
2023-02-04 08:38:54 +0200

    sup

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hell!");
+
+        Solution sol = new Solution();
+
+        int[] nums = new int[]{3, 3};
+        int target = 6;
+
+        System.out.println(Arrays.toString(sol.twoSum(nums, target)));
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] arr = new int[0];
+        int sum = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+
+            for (int j = 0; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    arr = new int[]{j, i};
+                    break;
+                }
+            }
+            break;
+        }
+        return arr;
     }
 }
